c723dc68db605fe2d0613e4d00b3adfbbc6873c7
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-05-26 21:36:11 +0530

    fixes

--- setup.sh
+++ setup.sh
@@ -1,16 +1,10 @@
 mkdir -p ~/.streamlit/
-echo "[server]
-headless=true
-port=$PORT
-enableCORS=false
 
-> ~/.streamlit/config.toml"
+echo "\
+[server]\n\
+headless=true\n\
+enableCORS=false\n\
+port=$PORT\n\
+"
+> ~/.streamlit/config.toml
 
-# mkdir -p ~/.streamlit/
-# echo "\
-# [server]\n\
-# headless = true\n\
-# port = $PORT\n\
-# enableCORS = false\n\
-# \n\
-# " > ~/.streamlit/config.toml
