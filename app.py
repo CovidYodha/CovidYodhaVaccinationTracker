6553ef4eda8c2a661f15b80081569ce7f8c63bfd
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2021-05-27 21:20:16 +0530

    fixes

--- app.py
+++ app.py
@@ -11,14 +11,17 @@ from copy import deepcopy
 
 st.set_page_config(layout='wide', initial_sidebar_state='collapsed')
 
+
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def load_mapping():
     df = pd.read_csv("district_mapping.csv")
     return df
 
+
 def filter_column(df, col, value):
     df_temp = deepcopy(df.loc[df[col] == value, :])
     return df_temp
+
 
 def filter_capacity(df, col, value):
     df_temp = deepcopy(df.loc[df[col] > value, :])
@@ -28,7 +31,7 @@ def filter_capacity(df, col, value):
 mapping_df = load_mapping()
 
 mapping_dict = pd.Series(mapping_df["district id"].values,
-                         index = mapping_df["district name"].values).to_dict()
+                         index=mapping_df["district name"].values).to_dict()
 
 rename_mapping = {
     'date': 'Date',
@@ -37,11 +40,11 @@ rename_mapping = {
     'vaccine': 'Vaccine',
     'pincode': 'Pincode',
     'name': 'Hospital Name',
-    'state_name' : 'State',
-    'district_name' : 'District',
+    'state_name': 'State',
+    'district_name': 'District',
     'block_name': 'Block Name',
-    'fee_type' : 'Fees'
-    }
+    'fee_type': 'Fees'
+}
 
 st.title('CoWIN Vaccination Slot Availability')
 st.info('The CoWIN APIs are geo-fenced so sometimes you may not see an output! Please try after sometime ')
@@ -64,11 +67,12 @@ date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
 date_str = [x.strftime("%d-%m-%Y") for x in date_list]
 
 temp_user_agent = UserAgent()
-browser_header = {'User-Agent': user_agent.random}
+browser_header = {'User-Agent': temp_user_agent.random}
 
 final_df = None
 for INP_DATE in date_str:
-    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(DIST_ID, INP_DATE)
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(
+        DIST_ID, INP_DATE)
     response = requests.get(URL, headers=browser_header)
     if (response.ok) and ('centers' in json.loads(response.text)):
         resp_json = json.loads(response.text)['centers']
@@ -76,11 +80,14 @@ for INP_DATE in date_str:
             df = pd.DataFrame(resp_json)
             if len(df):
                 df = df.explode("sessions")
-                df['min_age_limit'] = df.sessions.apply(lambda x: x['min_age_limit'])
+                df['min_age_limit'] = df.sessions.apply(
+                    lambda x: x['min_age_limit'])
                 df['vaccine'] = df.sessions.apply(lambda x: x['vaccine'])
-                df['available_capacity'] = df.sessions.apply(lambda x: x['available_capacity'])
+                df['available_capacity'] = df.sessions.apply(
+                    lambda x: x['available_capacity'])
                 df['date'] = df.sessions.apply(lambda x: x['date'])
-                df = df[["date", "available_capacity", "vaccine", "min_age_limit", "pincode", "name", "state_name", "district_name", "block_name", "fee_type"]]
+                df = df[["date", "available_capacity", "vaccine", "min_age_limit", "pincode",
+                         "name", "state_name", "district_name", "block_name", "fee_type"]]
                 if final_df is not None:
                     final_df = pd.concat([final_df, df])
                 else:
@@ -94,7 +101,8 @@ if (final_df is not None) and (len(final_df)):
     final_df.drop_duplicates(inplace=True)
     final_df.rename(columns=rename_mapping, inplace=True)
 
-    left_column_2, center_column_2, right_column_2, right_column_2a,  right_column_2b = st.beta_columns(5)
+    left_column_2, center_column_2, right_column_2, right_column_2a,  right_column_2b = st.beta_columns(
+        5)
     with left_column_2:
         valid_pincodes = list(np.unique(final_df["Pincode"].values))
         pincode_inp = st.selectbox('Select Pincode', [""] + valid_pincodes)
@@ -130,4 +138,3 @@ if (final_df is not None) and (len(final_df)):
     st.table(table)
 else:
     st.error("Unable to fetch data currently")
-
